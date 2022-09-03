99063d0e9e88ca71992867fbaf12cdb5871667c9
thajunnisaph <thajunnisaph123@gmail.com>
2022-09-04 00:50:08 +0530

    get by tagnametask5

--- dome.js
+++ dome.js
@@ -18,13 +18,27 @@ console.log(items);
 console.log(items[1]);
 items[1].textContent='hello';
 items[1].style.fontWeight='bold';
-//items[1].style.backgroundColor='yellow';
+items[1].style.backgroundColor='yellow';
 
 items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++)
 {
-   // items[i].style.backgroundColor='#f4f4f4';
+   items[i].style.backgroundColor='#f4f4f4';
     items[i].style.fontWeight='bold';
     items[i].style.color='blue';
+}
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+li[2].style.backgroundColor='green';
+for(var i=0;i<li.length;i++)
+ {
+   li[i].style.backgroundColor='#f4f4f4';
+   li[i].style.fontWeight='bold'; 
+   li[i].style.color='blue';
 }
 
